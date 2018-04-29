e5ccef2d8dc303fa9d798409c9fabf4faee584e6
adrielcavalcanti <cavalcantiadriel@gmail.com>
2018-04-28 22:30:22 -0300

    messages configs

--- app.js
+++ app.js
@@ -8,3 +8,27 @@ const Discord = require('discord.js');
 const client = new Discord.Client();
 const config = require('./config.json');
 const controller = require('./controller.js');
+
+// Server messages
+client.on('ready', () => {
+    console.log('Martinho Bot has started! :)');
+    console.log('Users: ' + client.users.size + ' | Guilds: ' + client.guilds.size);
+    client.user.setActivity('Ajudando ' + client.users.size + ' pessoas! :)');
+});
+
+client.on('guildCreate', guild => {
+    console.log('New guild joined: ' + guild.name + '(Users: ' + guild.memberCount + ', ID: '+ guild.id + ')');
+    client.user.setActivity('Ajudando ' + client.users.size + ' pessoas! :)');
+});
+
+client.on('guildDelete', guild => {
+    console.log('Guild deleted: ' + guild.name + '(ID: + ' + guild.id + ')');
+    client.user.setActivity('Ajudando ' + client.users.size + 'pessoas! :)');
+});
+
+// Users messages
+client.on('message', async message => {
+
+});
+
+client.login(config.token);
