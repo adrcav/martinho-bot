7a32e755434eb38523bb33d218162637894e4ccc
adrielcavalcanti <cavalcantiadriel@gmail.com>
2018-04-28 21:23:23 -0300

    added requires in app.js

--- app.js
+++ app.js
@@ -1,5 +1,10 @@
 /* 
-------------- Martinho - bot -----------------
-    By: Adriel Cavalcanti, Ítalo Sousa
-                  2018
+ * Martinho Bot
+ * Authors: Adriel Cavalcanti <adrcav>, Ítalo Sousa <italosa>
+ * 2018
 */
+
+const Discord = require('discord.js');
+const client = new Discord.Client();
+const config = require('./config.json');
+const controller = require('./controller.js');
